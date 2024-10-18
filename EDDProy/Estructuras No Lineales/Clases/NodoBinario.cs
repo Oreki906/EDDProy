321247d278210b363999f0f2a2348665175b03e2
flore <flore@LAPTOP-92DN0AH1>
2024-10-18 09:46:36 -0700

    se agrego un constructor

--- EDDProy/Estructuras No Lineales/Clases/NodoBinario.cs
+++ EDDProy/Estructuras No Lineales/Clases/NodoBinario.cs
@@ -7,9 +7,17 @@ using System.Threading.Tasks;
 namespace EDDemo.Estructuras_No_Lineales
 {
     public class NodoBinario
-    {
+    { 
         public int Dato;
         public NodoBinario Izq;
         public NodoBinario Der;
+
+        public NodoBinario(int Dato)
+        { 
+        this.Dato = Dato;
+        this.Izq = null;
+        this.Der = null;
+        //cambio 2
+        }
     }
 }
